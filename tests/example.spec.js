9dfd86787b9b9d5fb5e6ecde4bfcd8098a42166b
inehadev <n988420@gmail.com>
2026-04-15 23:15:40 +0530

    first test case

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,5 +2,19 @@
 import {test , expect} from '@playwright/test';
 
 test('has title' , async ({page})=>{
-  await page.goto('https://google.com');
+  await page.goto('https://amazon.com');
+  
 })
+
+test('first test case' , async ({page})=>{
+  expect(1000).toBe(1000);
+
+});
+
+test('second test case' , async  ({page})=>{
+  expect('neha').toContain("neha");
+});
+
+test('third test case', async ({page})=>{
+  expect("nehA").includes("neha").toBeTruthy();
+});
